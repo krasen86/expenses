3b310d7a507c6746c37f1948b532a82f04944636
Krasen Parvanov <krasenparvanov@gmail.com>
2021-02-19 22:51:24 +0100

    Add basic rest api controller for category endpoint/entity

---
+++ src/main/java/se/expense/controller/CategoryController.java
@@ -0,0 +1,57 @@
+package se.expense.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.expense.model.Category;
+import se.expense.repository.CategoryRepository;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    private CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository) {
+        super();
+        this.categoryRepository = categoryRepository;
+    }
+    
+    @GetMapping()
+    Collection<Category> getCategories() {
+        return categoryRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    ResponseEntity<?> getCategory(@PathVariable Long id) {
+        Optional<Category> category = categoryRepository.findById(id);
+        return category.map(response -> ResponseEntity.ok().body(response)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+
+    }
+
+    @PostMapping()
+    ResponseEntity<?> createCategory(@Valid @RequestBody Category category) throws URISyntaxException {
+        Category addedCategory = categoryRepository.save(category);
+        return ResponseEntity.created(new URI("/api/categories" + addedCategory.getId())).body(addedCategory);
+
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<?> updateCategory(@Valid @RequestBody Category category) throws URISyntaxException {
+        Category addedCategory = categoryRepository.save(category);
+        return ResponseEntity.ok().body(addedCategory);
+
+    }
+    @DeleteMapping("/{id}")
+    ResponseEntity<?> updateCategory(@PathVariable Long id)  {
+        categoryRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
--- src/main/java/se/expense/model/Category.java
+++ src/main/java/se/expense/model/Category.java
@@ -1,5 +1,6 @@
 package se.expense.model;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,7 +16,7 @@ public class Category {
 
     @Id
     private long id;
-
+    @NotNull
     private String categoryName;
 
 }
