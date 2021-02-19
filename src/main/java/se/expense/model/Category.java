7c6b3b116c363a57c18f074bb9510e44d5126495
Krasen Parvanov <krasenparvanov@gmail.com>
2021-02-19 21:22:49 +0100

    Add model for user and category

---
+++ src/main/java/se/expense/model/Category.java
@@ -0,0 +1,22 @@
+package se.expense.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    private long id;
+
+    private String categoryName;
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private User user;
+}
---
+++ src/main/java/se/expense/model/User.java
@@ -0,0 +1,29 @@
+package se.expense.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private long userID;
+    private String fistName;
+    private String lastName;
+    private String email;
+
+    @OneToMany
+    private Set<Category> categories;
+}
