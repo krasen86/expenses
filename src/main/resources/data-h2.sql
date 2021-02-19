fd84f3750b97f28ced2bbaae73d6e83b40b4bfd5
Krasen Parvanov <krasenparvanov@gmail.com>
2021-02-19 22:11:20 +0100

    Add test data in sql file and update properties in order to execute the queries att start

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,13 @@
+insert into users values (1,'Test 1','Test 1', 'test1@test.com');
+insert into users values (2,'Test 2', 'Test 2', 'test2@test.com');
+insert into users values (3,'Test 3', 'Test 3', 'test3@test.com');
+
+
+insert into categories values (1,'Category 1');
+insert into categories values (2,'Category 2');
+insert into categories  values (3,'Category 3');
+
+
+insert into expenses values (1,'My first expense','2021-02-10T17:00:00.000Z',1,3);
+insert into expenses values (2,'My second expense','2021-02-16T15:00:00.000Z',2,1);
+insert into expenses values(3,'My third expense','2021-02-19T15:00:00.000Z',3,2);
